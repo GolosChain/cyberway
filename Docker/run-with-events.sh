e4c4679c2ed0a2b551f752adffd891d43396a3af
Semen Medvedev <smedvedev@golos.io>
2019-02-06 16:26:40 +0700

    Add script to start cyberway node as docker-compose project #277

--- Docker/run-with-events.sh
+++ Docker/run-with-events.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+cd $(dirname $(readlink -f $0))
+
+if [[ "$2" -eq "clenup" ]]; then
+    docker-compose -f docker-compose-events.yml down || exit 1
+    docker volume rm cyberway-mongodb-data || exit 1
+    docker volume rm cyberway-nodeos-data || exit 1
+    docker volume rm cyberway-queue || exit 1
+fi
+
 docker volume create cyberway-mongodb-data || true
 docker volume create cyberway-nodeos-data || true
-docker volume create cyberway-keosd-data || true
 docker volume create cyberway-queue || true
 
 EXPORTER_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
