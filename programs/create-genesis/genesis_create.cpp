95a9297869d8668dd2d50fa38de0ed4b24aa4448
Semen Medvedev <smedvedev@golos.io>
2019-04-30 13:00:23 +0700

    Initialize id in constructor #584

--- programs/create-genesis/genesis_create.cpp
+++ programs/create-genesis/genesis_create.cpp
@@ -523,9 +523,9 @@ struct genesis_create::genesis_create_impl final {
             s.enabled = false;      // ?
         });
         db.start_section(config::system_account_name, N(stake.param), "param_struct", 1);
-        db.set_autoincrement<stake_param_object>(sys_sym.to_symbol_code());
         db.emplace<stake_param_object>([&](auto& p) {
             const auto inf = _info.params.stake;
+            p.id = sys_sym.to_symbol_code().value;
             p.token_symbol = sys_sym;
             p.max_proxies = inf.max_proxies;
             p.payout_step_length = inf.payout_step_length;
